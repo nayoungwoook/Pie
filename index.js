7a735e0b62834e457a8577e056751e9026c9cd8e
nayoungwook <nayoungwoook@gmail.com>
2022-11-27 13:01:51 +0900

    nov 27 done :D

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(server);
 module.exports = { io };
 
 const { User } = require('./server/user');
-var { Room, rooms, makeRoom, getRoom } = require("./server/room");
+const { Room, rooms, makeRoom, getRoom } = require("./server/room");
 
 app.use(express.static('client'));
 app.get('/', (req, res) => {
